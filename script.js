ebe56acd2a2c8c3aca1686fa9b4368f10ce0bd62
Chris Wise <chris.wise@doordash.com>
2025-01-22 14:36:14 -0800

    removing unneeded dashpass field

--- script.js
+++ script.js
@@ -140,13 +140,6 @@ document.addEventListener("DOMContentLoaded", () => {
                     <option value="Yes">Yes</option>
                     <option value="No">No</option>
                 </select>
-        
-                <label for="dashPassOnly">DashPass Only?</label>
-                <select id="dashPassOnly" class="input-field boolean-field">
-                    <option value="">Select...</option>
-                    <option value="Yes">Yes</option>
-                    <option value="No">No</option>
-                </select>
             `;
         } else if (promoType === "bogo") {
             promoInputsContainer.innerHTML += `
